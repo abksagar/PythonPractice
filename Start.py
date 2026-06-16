dc31065ac3d8e31b27f6b02f3ce1fee52411267a
Ankit <abksagar@gmail.com>
2026-06-16 22:33:47 +0530

    changefile

--- Start.py
+++ Start.py
@@ -28,3 +28,26 @@ print("type of a is:",type(a))
 print("type of b is:",type(b))
 
 print(id(a)==id(b))
+
+a=(10,"Jay Ganesh",4.15)
+a=[10,"Jay Ganesh",4.15]
+print("Value of a is :",a)
+print("id of a is:",id(a))  
+print("type of a is:",type(a))
+a[0]=20   # we cannot change the value of tuple because it is immutable
+print(a[0])
+
+
+x=10
+print(hash(x))
+
+s="Jay Ganesh"
+print(hash(s))
+
+c=10.9
+print(hash(c))
+
+print(hash(x))
+
+
+
