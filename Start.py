11cd2dab9784aaf29af7d8435e95951a5624c045
Ankit <abksagar@gmail.com>
2026-06-17 01:14:12 +0530

    changefiletocheckidfunc

--- Start.py
+++ Start.py
@@ -50,4 +50,52 @@ print(hash(c))
 print(hash(x))
 
 
+x = [1, 2, 3]
+y = [1, 2, 3]
+print(id(x))          # Different address
+print(id(y)) 
 
+
+x = (1, 2, 3)
+y = (1, 2, 3)
+print(id(x))         # Same address
+print(id(y)) 
+
+a="A"
+b="A"
+print(id(a))         # Same address
+print(id(b))
+
+
+n=3000
+m=3000
+print(id(n))         # same address    
+print(id(m))
+
+
+j={"Name":"Jay Ganesh",
+   "Age":24,}
+
+k={"Name":"Jay Ganesh",
+   "Age":24,}
+
+print(id(j))         # Different address
+print(id(k))
+
+
+bytes1=([65,66,67])
+bytes2=([65,66,67])
+print(id(bytes1))         # Different address
+print(id(bytes2))
+
+
+f="Jay Ganesh"
+print(id(f))         # Same address
+g="Jay Ganesh"
+print(id(g))         # Same address
+
+
+s=58952145555
+v=58952145555
+print(id(s))         # same address
+print(id(v))         # same address
